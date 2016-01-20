85c318e9783d44a29da3a2755cbd2485d1f6f58a
Paul Zaich <pzaich@gmail.com>
2016-01-20 14:45:51 -0800

    re-arrange cards in dashboard state to reflect mocks + update class of dashboard module to tall

--- app/assets/javascripts/components/fifa/dashboard/modules/insights_implications.js.jsx
+++ app/assets/javascripts/components/fifa/dashboard/modules/insights_implications.js.jsx
@@ -2,7 +2,7 @@ var InsightsImplications = React.createClass({
   render: function() {
     var hiddenStyle = this.props.hidden ? {display: 'none'} : {};
     return (
-      <div id="insights_implications" className="dashboard-module" style={hiddenStyle}>
+      <div id="insights_implications" className="dashboard-module tall" style={hiddenStyle}>
         <div className="top">
           <div className="drag-handle"></div>
           <div className="top-title">Insights & Implications</div>
