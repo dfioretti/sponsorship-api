5d3bd6e18623f0da59f89a01ab8a89a62d0e6c32
Paul Zaich <pzaich@gmail.com>
2016-01-28 12:47:24 -0800

    cartoDB query migrated to utilize clusters, but not loading

--- app/assets/javascripts/components/fifa/dashboard/modules/global_hotspots.js.jsx
+++ app/assets/javascripts/components/fifa/dashboard/modules/global_hotspots.js.jsx
@@ -12,17 +12,40 @@ var GlobalHotspots = React.createClass({
         attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
     }).addTo(map);
 
-    var clusterSQL = $('#sql_template_a').html();
+    var clusterSQL = $('#global_hotspots_sql').html();
 
-    cartodb.createLayer(map, {
-      user_name:'amandacoston',
-      type:'cartodb',
-      sublayers:[{
-        sql: clusterSQL,
-        cartocss:"#layer{  marker-width: 12;  marker-fill: #5CA2D1;  marker-line-width: 1.5;  marker-fill-opacity: 0.8;  marker-line-opacity: 0;  marker-line-color: #fff; marker-allow-overlap: true;   }   ",
-      }]
-    })
-    .addTo(map);
+    // cartodb.createLayer(map, {
+    //   user_name:'amandacoston',
+    //   type:'cartodb',
+    //   sublayers:[{
+    //     sql: clusterSQL,
+    //     // cartocss: "#layer{  marker-width: 12;  marker-fill: #5CA2D1;  marker-line-width: 1.5;  marker-fill-opacity: 0.8;  marker-line-opacity: 0;  marker-line-color: #fff; marker-allow-overlap: true;  [src = 'smalls'] {marker-line-width: 0;    marker-width: 12;  }   [src = 'mids'] {    marker-line-width: 0;   marker-width:36;  }  [src = 'bigs']    {marker-line-width: 0;    marker-width: 48;  } [ average <= 35676000] {   marker-fill: #BD0026;}  [ average <= 300424] {   marker-fill: #F03B20;}  [ average <= 111461] {   marker-fill: #FD8D3C;}  [ average <= 47653] {   marker-fill: #FECC5C;}  [ average <= 15601] {   marker-fill: #FFFFB2;}  }"
+    //     cartocss:"#layer{  marker-width: 12;  marker-fill: #5CA2D1;  marker-line-width: 1.5;  marker-fill-opacity: 0.8;  marker-line-opacity: 0;  marker-line-color: #fff; marker-allow-overlap: true;   }   ",
+    //   }]
+    // })
+    // .done(function (e) {
+    //   console.log(e)
+    // })
+    // .error(function (e) {
+    //   console.log(e)
+    // })
+    // .on('done', function(layer){
+    //  cdb.vis.Vis.addInfowindow(map,layer.getSubLayer(0), ['points_count', 'average'],{infowindowTemplate: $('#infowindow_template').html()});
+    // })
+    // .addTo(map);
+
+    var sql = new cartodb.SQL({ user: 'amandacoston' });
+
+    sql.execute(clusterSQL)
+      .done(function(data) {
+        console.log(data.rows);
+      })
+      .error(function(errors) {
+        // errors contains a list of errors
+        console.log(errors)
+        console.log("errors:  " + errors);
+      });
+
   },
   renderMap: function() {
     return <div id="map" className="carto-map"></div>
@@ -43,8 +66,51 @@ var GlobalHotspots = React.createClass({
           </div>
           {this.renderMap()}
         </div>
-        <script type="sql/html" id="sql_template_a">
-          SELECT * FROM sojcp0flmlm7rshw6usbx27
+        <script type="sql/html" id="global_hotspots_sql">
+          WITH
+              hgridA AS (
+                  SELECT CDB_HexagonGrid(ST_Expand(!bbox!, greatest(!pixel_width!,!pixel_height!) * 48),
+                  greatest(!pixel_width!,!pixel_height!) * 48) as cell),
+
+              bigs AS (
+                  SELECT * FROM (SELECT ST_Centroid(ST_Collect(i.the_geom_webmercator)) as the_geom_webmercator,
+                      count(i.cartodb_id) as points_count,
+                      1 as cartodb_id, array_agg(cartodb_id) AS id_list,
+                      avg(sentiment_score) AS average FROM hgridA,
+                      (select * from sojcp0flmlm7rshw6usbx27) i where ST_Intersects(i.the_geom_webmercator, hgridA.cell)
+                          GROUP BY hgridA.cell) t WHERE points_count > 100 ),
+
+              hgridB AS (
+                  SELECT CDB_HexagonGrid(ST_Expand(!bbox!, greatest(!pixel_width!,!pixel_height!) * 36),
+                      greatest(!pixel_width!,!pixel_height!) * 36) as cell),
+
+              mids AS (
+                  SELECT * FROM (SELECT ST_Centroid(ST_Collect(i.the_geom_webmercator)) as the_geom_webmercator,
+                      count(i.cartodb_id) as points_count, 1 as cartodb_id, array_agg(cartodb_id) AS id_list,
+                      avg(sentiment_score) AS average FROM hgridB,
+                      (select * from sojcp0flmlm7rshw6usbx27) i where ST_Intersects(i.the_geom_webmercator, hgridB.cell) AND cartodb_id NOT IN (SELECT unnest(id_list) FROM bigs)
+                          GROUP BY hgridB.cell) t WHERE points_count > 25 ),
+
+              hgridC AS (
+                  SELECT CDB_HexagonGrid(ST_Expand(!bbox!, greatest(!pixel_width!,!pixel_height!) * 12),
+                      greatest(!pixel_width!,!pixel_height!) * 12) as cell),
+
+              smalls AS (
+                  SELECT * FROM (
+                      SELECT ST_Centroid(ST_Collect(i.the_geom_webmercator)) as the_geom_webmercator,
+                      count(i.cartodb_id) as points_count, 1 as cartodb_id, array_agg(cartodb_id) AS id_list,
+                      avg(sentiment_score) AS average FROM hgridC,
+                      (select * from sojcp0flmlm7rshw6usbx27) i where ST_Intersects(i.the_geom_webmercator, hgridC.cell) AND cartodb_id NOT IN (SELECT unnest(id_list) FROM bigs) AND cartodb_id NOT IN (SELECT unnest(id_list) FROM mids)
+                          GROUP BY hgridC.cell) t WHERE points_count > 1 )
+
+          SELECT the_geom_webmercator, 1 points_count, cartodb_id, ARRAY[cartodb_id], sentiment_score AS average, 'origin' as src FROM sojcp0flmlm7rshw6usbx27 WHERE cartodb_id NOT IN (select unnest(id_list) FROM bigs) AND cartodb_id NOT IN (select unnest(id_list) FROM mids) AND cartodb_id NOT IN (select unnest(id_list) FROM smalls)
+
+          UNION ALL
+          SELECT *, 'bigs' as src FROM bigs
+          UNION ALL
+          SELECT *, 'mids' as src FROM mids
+          UNION ALL
+          SELECT *, 'smalls' as src FROM smalls
         </script>
       </div>
     );
