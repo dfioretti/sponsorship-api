af3568a8c6a6d38e34c3ed92d610653bdc34e422
Paul Zaich <pzaich@gmail.com>
2016-02-04 17:28:55 -0800

    fixes dashboard toggling by forcing second render of component

--- app/assets/javascripts/components/utils/image_with_fallback.jsx
+++ app/assets/javascripts/components/utils/image_with_fallback.jsx
@@ -6,7 +6,6 @@ var ImageWithFallback = React.createClass({
     this.setState({ errored: true });
   },
   render: function (argument) {
-    console.log(this.props.fallbackSrc)
     var src = this.state.errored ? this.props.fallbackSrc : this.props.src;
 
     return (<img src={src} onError={this.errorHandler}/>);
