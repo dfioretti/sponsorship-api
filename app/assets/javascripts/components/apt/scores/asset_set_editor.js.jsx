e4bd34b29b866c41c3086f74f1903b8b737700b1
David Fioretti <fioretti.david@gmail.com>
2016-02-24 17:42:36 -0500

    making progress on asset select

--- app/assets/javascripts/components/apt/scores/asset_set_editor.js.jsx
+++ app/assets/javascripts/components/apt/scores/asset_set_editor.js.jsx
@@ -4,11 +4,19 @@ var RouteHandler = ReactRouter.RouteHandler,
 var AssetSetEditor = React.createClass({
   mixins: [DashboardMixin],
   getInitialState: function() {
-    return { scoreLoaded: false };
+    return { assetsLoaded: false };
   },
   componentWillMount: function() {
+    this.props.setTitle('Asset Set Editor');
+    DashboardsStore.getAsset(7).then(function() {
+      this.setupGrid();
+    }.bind(this));
   },
   componentWillReceiveProps: function(newProps) {
+    DashboardsStore.getAsset(newProps.params.id).then(function() {
+      this.handleChange();
+      $('.modules-container').trigger('ss-rearrange');
+    }.bind(this));
   },
   mapModule: function(name, state) {
 
@@ -25,6 +33,7 @@ var AssetSetEditor = React.createClass({
         <div className="dashboard">
           <Sidebar {...this.props} dashboardState={""} dashboardType="score-editor" />
           <div className="modules-box">
+            {this.renderModules(null)}
           </div>
         </div>
       );
--- app/assets/javascripts/components/apt/scores/asset_table_select.js.jsx
+++ app/assets/javascripts/components/apt/scores/asset_table_select.js.jsx
@@ -1,30 +1,80 @@
 var AssetTableSelect = React.createClass({
   componentDidMount: function() {
-    // hack to make sure the dom is rendered
-    // before i setup the canvas, call to score.js
-    // moving this to component because that's where i'm doing
-    // the updates..
-    //window.setTimeout(initilizeScoreCanvas, 2000);
+    $('#asset-table').DataTable(
+      {
+        "bPaginate": false
+      }
+    );
+    $('#asset-table tbody').on('click', 'tr', function() {
+      $(this).toggleClass('asset-selected');
+    })
   },
-  // wrappers for code in score.js
-  render: function(){
-    var overrideStyle = { };
+  componentWillMount: function() {
+    AssetsStore.setCurrent();
+
+    AssetsStore.on("update", function() {
+      this.setState({loaded: true});
+    }.bind(this));
+  },
+  renderList: function() {
+    var assets = AssetsStore.getState().assets;
+    console.log(assets);
+
+    var list = $.map(assets, function(asset) {
+      console.log(asset);
+      return (
+        <tr>
+          <td>
+            {asset.name}
+          </td>
+          <td>
+            {asset.scope}
+          </td>
+          <td>
+            {asset.category}
+          </td>
+          <td>
+            {asset.subcategory}
+          </td>
+        </tr>
+      );
+    }.bind(this));
+    return (
+      <tbody>
+        {list}
+      </tbody>
+    );
+
+  },
+  render: function() {
+    var overrideStyle = {};
     var iconStyle = {
-      fontSize: "20px"
+      fontSize: "20px",
     };
 
     return (
-      <div id="tree-editor" className="dashboard-module huge" style={overrideStyle}>
-          <div className="top">
-            <div className="drag-handle">
-            </div>
-            <div className="top-title">
-              Score Builder
-            </div>
+      <div className="dashboard-module huge" style={overrideStyle}>
+        <div className="top">
+          <div className="drag-handle"></div>
+          <div className="top-title">
+            Score Builder
           </div>
-          <div className="main">
-            In it
-          </div>
+        </div>
+        <div className="main">
+          <div className="asset-table-container">
+          <table id="asset-table" className="table table-hover asset-table" cellspacing="2" width="100%">
+            <thead>
+              <tr>
+                <th>Asset</th>
+                <th>Scope</th>
+                <th>Category</th>
+                <th>Subcategory</th>
+              </tr>
+            </thead>
+            {this.renderList()}
+          </table>
+        </div>
+        </div>
 
       </div>
     );
