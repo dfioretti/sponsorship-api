979e1c2bc1b92255d29876f894c303905fe9432a
David Fioretti <fioretti.david@gmail.com>
2016-03-01 21:29:09 -0500

    adding dummy text

--- app/assets/javascripts/components/apt/asset/dashboard/modules/asset_overview.js.jsx
+++ app/assets/javascripts/components/apt/asset/dashboard/modules/asset_overview.js.jsx
@@ -8,6 +8,7 @@ var AssetOverview = React.createClass({
     var hiddenStyle = this.props.hidden ? {display: 'none'} : {};
     var asset = this.props.asset;
     var imageUrl = "/images/" + asset.id + ".jpg";
+    var description = (asset.description == null) ? "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." : asset.description;
     return (
       <div id="asset_overview" className="dashboard-module" style={hiddenStyle}>
         <div className="top">
@@ -36,7 +37,7 @@ var AssetOverview = React.createClass({
             <div className="back small-padding">
               <h4>Asset Bio</h4>
               <p>
-              {asset.description}
+              {description}
               </p>
             </div>
           </div>
