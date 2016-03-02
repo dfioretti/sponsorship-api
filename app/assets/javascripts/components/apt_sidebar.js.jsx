dc8ad6dacdadc2062fd18ab449aa3ac9754b6c84
David Fioretti <fioretti.david@gmail.com>
2016-03-01 23:52:25 -0500

    still playing with this dumb menu

--- app/assets/javascripts/components/apt_sidebar.js.jsx
+++ app/assets/javascripts/components/apt_sidebar.js.jsx
@@ -10,44 +10,70 @@ var AptSidebar = React.createClass({
 
   },
   renderContent: function() {
+    return (
+      <div className="menu-toggle-container">
+      </div>
+    )
+  },
+  toggleMenu: function() {
+    console.log("called");
+    $('#collapse-menu').slideToggle(250);
+  },
+  renderMenu: function() {
     var createLink = '/apt/dashboard_detail';
     var scoresLink = '/apt/scores/score_index';
     var assetSetLink = "/apt/scores/asset_set_index";
+    var createScore = '/apt/scores/score_editor/1';
+    var createAssetSets = '/apt/scores/asset_set_editor/1'
     //<button  style={{width: "100%"}} className="btn btn-primary">Create Component</button>
 
     return (
-      <div>
+      <div className="top">
       <div style={{margin: "10px"}}>
-        <button type="button" className="invisible-button" data-toggle="collapse" data-target="#collapse-menu" aria-expanded="false" aria-controls="collapse-menu" aria-label="Menu Hamburger">
+        <button onClick={this.toggleMenu}type="button" className="invisible-button"  aria-label="Menu Hamburger">
           <span style={{color: "white", fontSize: "22px", textAlign: "center"}}className="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span>
         </button>
         <span style={{color: "white", lineHeight: "14px", letterSpacing: "1.5px", verticalAlign: "text-top", paddingLeft: "10px", fontSize: "14px"}}>MENU</span>
       </div>
 
-      <div style={{width: "100%", backgroundColor: "white"}}className="collapse" id="collapse-menu">
-        <div style={{paddingTop: "0px"}}className="sidebar-content">
+      <div style={{width: "100%", backgroundColor: "#111316", display: "none"}}className="collapse" id="collapse-menu">
+        <div style={{paddingLeft: "10px"}}className="sidebar-content">
           <br />
           <Link to={createLink}>
             Create Component
           </Link><br /><br />
+        <Link to={createScore}>
+          Create New Score
+          </Link><br /><br />
           <Link to={scoresLink}>
             View Scores
           </Link><br /><br />
+          <Link to={createAssetSets}>
+            Create Asset Set
+          </Link><br /><br />
           <Link to={assetSetLink}>
-            Asset Sets
+            View Asset Sets
           </Link><br /><br />
         </div>
-      </div>
-      <div style={{margin: "10px"}}className="sidebar-content">
       </div>
     </div>
     );
   },
   render: function() {
+    //{this.renderTop()}
+
       return (
         <div className="sidebar">
-          {this.renderTop()}
+          {this.renderMenu()}
           {this.renderContent()}
+          <div className="print-report">
+            <ul>
+              <li>
+                <div className="gear"></div>
+                <div className="icon print-report">Print Report</div>
+              </li>
+            </ul>
+          </div>
         </div>
       );
   }
