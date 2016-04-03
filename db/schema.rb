db2e69b84b97ebf743689a8a9bb4c6d9a9bc9473
David Fioretti <fioretti.david@gmail.com>
2016-04-03 06:38:15 -0400

    ok

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403061354) do
+ActiveRecord::Schema.define(version: 20160403103608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -151,6 +151,17 @@ ActiveRecord::Schema.define(version: 20160403061354) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "attachment"
+  end
+
+  create_table "que_jobs", id: false, force: :cascade do |t|
+    t.integer  "priority",    limit: 2, default: 100,                                        null: false
+    t.datetime "run_at",                default: "now()",                                    null: false
+    t.integer  "job_id",      limit: 8, default: "nextval('que_jobs_job_id_seq'::regclass)", null: false
+    t.text     "job_class",                                                                  null: false
+    t.json     "args",                  default: [],                                         null: false
+    t.integer  "error_count",           default: 0,                                          null: false
+    t.text     "last_error"
+    t.text     "queue",                 default: "",                                         null: false
   end
 
   create_table "scores", force: :cascade do |t|
