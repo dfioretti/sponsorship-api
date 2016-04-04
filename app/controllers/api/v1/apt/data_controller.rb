7f56e0e791c522d7f017f11ac3386c1067caa770
David Fioretti <fioretti.david@gmail.com>
2016-04-04 19:39:07 -0400

    uah

--- app/controllers/api/v1/apt/data_controller.rb
+++ app/controllers/api/v1/apt/data_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::Apt::DataController < ApplicationController
   def index
-    @data = Datum.where(:active => true)
-    render json: @data
+    if params.has_key?(:metrics)
+      @data = Metric.all.limit(500)
+      render json: @data
+    else
+      @data = Datum.where(:active => true)
+      render json: @data
+    end
   end
 end
