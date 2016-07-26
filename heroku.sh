63d480a25c14968389dc38381c85e8a237cf010d
David Fioretti <fioretti.david@gmail.com>
2016-07-26 14:41:54 -0400

    update heroku script

--- heroku.sh
+++ heroku.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 cp -r ~/code/teneo-apps/sponsorship-client/public/ ~/code/teneo-apps/sponsorship-api/public/
+git add .
+git commit -a -m "new app build"
+git push origin multi
+git push heroku multi:master
+
+heroku pg:reset DATABASE --confirm teneosponsorship
+heroku pg:push teneo_ews_development --app teneosponsorship DATABASE
